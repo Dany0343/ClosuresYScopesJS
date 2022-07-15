4cbb9beeb8b0d54f5130ff024b6f4eb61ee36be9
Oscar Bucio <oscarbucio2001@gmail.com>
2022-07-15 00:28:00 -0500

    done

---
+++ src/debug/index.js
@@ -0,0 +1,11 @@
+// var a = 'Helo';
+
+// function hello() {
+//     let b = "Hello world";
+//     const c = 'Hello world!!';
+
+//     debugger;
+// }
+
+// hello();
+const scope = "I am global";
---
+++ src/debug/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+const scope = "I am global";
